3391043a821c7804a207efb091ef6f65f05ed51c
q6crypto <q6crypto@gmail.com>
2023-09-13 21:35:20 -0400

    fix oracle

--- contracts/oracle.sol
+++ contracts/oracle.sol
@@ -4,67 +4,74 @@ pragma solidity 0.8.17;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./interfaces/IPool.sol";
 
-contract Oracle is Ownable {
+import "hardhat/console.sol";
 
+contract Oracle is Ownable {
     address public token0;
     address public token1;
     IPool public pair;
     uint256 public window = 12;
     uint256 public windowTwap = 2;
 
-        constructor(
-        IPool _pair
-        )  {
+    constructor(IPool _pair) {
         pair = _pair;
         token0 = pair.token0();
         token1 = pair.token1();
+    }
 
-        }
-
-    function consult(address _token, uint256 _amountIn) external view returns (uint256 amountOut) {
+    function consult(
+        address _token,
+        uint256 _amountIn
+    ) external view returns (uint256 amountOut) {
         uint256 currentWindow;
         uint256 lenght = pair.observationLength();
-        if(lenght < 1) {
-        amountOut = 1 ether;    
+        if (lenght < 1) {
+            amountOut = 1 ether;
         }
-        if(lenght <= window) {
-        currentWindow = lenght -1;    
+        if (lenght < window) {
+            currentWindow = lenght;
         }
-        if(lenght > window) {
-        currentWindow = window;    
+        if (lenght >= window) {
+            currentWindow = window;
         }
         if (_token == token0) {
-            amountOut = pair.sample(token0,_amountIn,1,currentWindow)[0];
+            amountOut = pair.sample(token0, _amountIn, 1, currentWindow)[0];
         } else {
             require(_token == token1, "Oracle: INVALID_TOKEN");
-            amountOut = pair.sample(token1,_amountIn,1,currentWindow)[0];
+            amountOut = pair.sample(token1, _amountIn, 1, currentWindow)[0];
         }
     }
 
-    function twap(address _token, uint256 _amountIn) external view returns (uint256 amountOut) {
+    function twap(
+        address _token,
+        uint256 _amountIn
+    ) external view returns (uint256 amountOut) {
         uint256 currentWindow;
         uint256 lenght = pair.observationLength();
-        if(lenght == 1) {
-        amountOut = 1 ether;    }
-        if(lenght <= windowTwap) {
-        currentWindow = lenght -1;    
+        if (lenght == 1) {
+            amountOut = 1 ether;
         }
-        if(lenght > windowTwap) {
-        currentWindow = windowTwap;    
+        if (lenght <= windowTwap) {
+            currentWindow = lenght - 1;
+        }
+        if (lenght > windowTwap) {
+            currentWindow = windowTwap;
         }
         if (_token == token0) {
-            amountOut = pair.sample(token0,_amountIn,1,currentWindow)[0];
+            amountOut = pair.sample(token0, _amountIn, 1, currentWindow)[0];
         } else {
             require(_token == token1, "Oracle: INVALID_TOKEN");
-            amountOut = pair.sample(token1,_amountIn,1,currentWindow)[0];
+            amountOut = pair.sample(token1, _amountIn, 1, currentWindow)[0];
         }
     }
 
-    function setWindows(uint256 _window, uint256 _windowTwap) external onlyOwner {
-            window = _window;
-            windowTwap = _windowTwap;
+    function setWindows(
+        uint256 _window,
+        uint256 _windowTwap
+    ) external onlyOwner {
+        window = _window;
+        windowTwap = _windowTwap;
     }
-
 
     function setPair(IPool _pair) external onlyOwner {
         require(address(_pair) != address(0), "Oracle: Invalid pair address");
@@ -72,5 +79,4 @@ contract Oracle is Ownable {
         token0 = pair.token0();
         token1 = pair.token1();
     }
-
 }
