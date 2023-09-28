7e3a91040062befcd95716981bdd5fc8b3083e3e
tee <pulserateX@proton.me>
2023-09-29 01:54:06 +0200

    Update CommunityFundV2.sol

--- contracts/CommunityFundV2.sol
+++ contracts/CommunityFundV2.sol
@@ -12,6 +12,14 @@ contract CommunityFundV2 is AccessControl {
     using Address for address payable;
     using SafeERC20 for IERC20;
 
+    event ExecuteTransaction(
+        bytes32 indexed txHash,
+        address indexed target,
+        uint value,
+        string signature,
+        bytes data
+    );
+
     bytes32 public constant CALLER_ROLE = keccak256("CALLER_ROLE");
 
     constructor() {
@@ -37,6 +45,35 @@ contract CommunityFundV2 is AccessControl {
         IERC20 token = IERC20(tokenAddress);
         token.safeTransfer(to, token.balanceOf(address(this)));
     }
+
+    
+    // to interact with other contracts
+    function sendCustomTransaction(
+        address target,
+        uint value,
+        string memory signature,
+        bytes memory data
+    ) public payable onlyRole(CALLER_ROLE)  returns (bytes memory) {
+        bytes32 txHash = keccak256(abi.encode(target, value, signature, data));
+        bytes memory callData;
+        if (bytes(signature).length == 0) {
+            callData = data;
+        } else {
+            callData = abi.encodePacked(
+                bytes4(keccak256(bytes(signature))),
+                data
+            );
+        }
+        (bool success, bytes memory returnData) = target.call{value: value}(
+            callData
+        );
+        require(success, "Transaction execution reverted.");
+
+        emit ExecuteTransaction(txHash, target, value, signature, data);
+
+        return returnData;
+    }
+
 
     receive() external payable {}
 }
